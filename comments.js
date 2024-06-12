34a7a1974323e8e4b4e03eb5fe7e0c25569af1c1
meghachi <102932379+meghachi@users.noreply.github.com>
2024-06-12 04:28:15 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,27 @@
+//Create web server
+const express = require('express')
+const app = express()
+const port = 3000
+
+// Use express.json() to parse incoming JSON data
+app.use(express.json())
+
+// Create an array to store the comments
+let comments = []
+
+// Create a route to get all the comments
+app.get('/comments', (req, res) => {
+  res.json(comments)
+})
+
+// Create a route to create a new comment
+app.post('/comments', (req, res) => {
+  const comment = req.body
+  comments.push(comment)
+  res.status(201).json(comment)
+})
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Express app listening on http://localhost:${port}`)
+})

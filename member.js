7843fc5ff94aca008b41b9c3e3fe6d9173941c01
meghachi <102932379+meghachi@users.noreply.github.com>
2024-06-12 04:22:28 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,24 @@
+function skillsMember() {
+    var skills = [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React",
+        "Redux",
+        "Node",
+        "Express",
+        "MongoDB",
+        "Firebase",
+        "Git",
+        "GitHub",
+        "Heroku",
+        "Netlify",
+        "VS Code",
+        "Figma",
+        "Adobe XD",
+        "Photoshop",
+        "Illustrator"
+    ];
+
+    return skills;
+}
